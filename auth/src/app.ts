438d1003f77ea14db20dfc5e26d40784afcce250
Khanh Duong <phuckhanh22@gmail.com>
2022-08-17 01:14:45 +0700

    convert some service to personal node_module

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import { currentUserRouter } from './routes/current-user'
 import { signupRouter } from './routes/signup'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found-error'
+import { errorHandler, NotFoundError } from '@khanhdp955/common'
 
 const app = express()
 app.set('trust proxy', true)
